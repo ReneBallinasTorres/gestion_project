e0d7d889c17257fe3e482cbf7eaaf01adbcc0127
Ren <rene10torres10@gmail.com>
2025-04-10 08:00:39 -0600

    Dashborad Perincipales

--- Admin/Dashboard.php
+++ Admin/Dashboard.php
@@ -34,6 +34,23 @@ if (!$Admin) {
 mysqli_close($conexion);
 ?>
 
+<?php
+// Conexión a la BD (reabrimos para las consultas)
+include '../connection/connection.php';
+
+// Contar usuarios
+$consulta_usuarios = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM usuarios");
+$total_usuarios = mysqli_fetch_assoc($consulta_usuarios)['total'];
+
+// Contar proyectos
+$consulta_peroyectos = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM proyectos");
+$total_proyectos = mysqli_fetch_assoc($consulta_peroyectos)['total'];
+
+// Contar equipos
+$consulta_equipos = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM equipos");
+$total_equipos = mysqli_fetch_assoc($consulta_equipos)['total'];
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -48,8 +65,13 @@ mysqli_close($conexion);
     <link rel="stylesheet" href="../Styles/style_dashboard.css">
 </head>
 
-<body>
+<style>
+    .card {
+        border-radius: 1rem;
+    }
+</style>
 
+<body>
     <!-- Sidebar -->
     <div class="sidebar">
         <h2>Admin Panel</h2>
@@ -75,6 +97,134 @@ mysqli_close($conexion);
                 </div>
             </div>
         </nav>
+
+        <div class="container mt-4">
+            <div class="row g-3">
+                <div class="row g-3">
+                    <!-- Cuadro de Total de Usuarios -->
+                    <div class="col-md-12">
+                        <div class="card text-dark bg-light mb-3 shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Usuarios Registrados (Total: <?php echo $total_usuarios; ?>)</h5>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-hover">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th>ID Usuario</th>
+                                                <th>Nombre</th>
+                                                <th>Apellido Paterno</th>
+                                                <th>Apellido Materno</th>
+                                                <th>Edad</th>
+                                                <th>Correo</th>
+                                                <th>Contraseña</th>
+                                                <th>Rol</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Obtener todos los usuarios con su rol
+                                            $resultado_usuarios = mysqli_query($conexion, "SELECT u.*, r.n_rol 
+                                            FROM usuarios u JOIN roles r ON u.id_rol = r.id_rol LIMIT 3");
+                                            while ($usuario = mysqli_fetch_assoc($resultado_usuarios)) : ?>
+                                                <tr>
+                                                    <td><?php echo $usuario['id_usuario']; ?></td>
+                                                    <td><?php echo $usuario['n_usuario']; ?></td>
+                                                    <td><?php echo $usuario['a_p']; ?></td>
+                                                    <td><?php echo $usuario['a_m']; ?></td>
+                                                    <td><?php echo $usuario['edad']; ?></td>
+                                                    <td><?php echo $usuario['correo']; ?></td>
+                                                    <td><?php echo $usuario['pass']; ?></td>
+                                                    <td><?php echo $usuario['n_rol']; ?></td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <a href="Dashboard_usuario.php" class="btn btn-primary btn-sm">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Cuadro de Proyectos -->
+                    <div class="col-md-6">
+                        <div class="card text-dark bg-light mb-3 shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Proyectos Creados (Total: <?php echo $total_proyectos; ?>)</h5>
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hover">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nombre</th>
+                                                <th>Descripción</th>
+                                                <th>Fecha Inicial</th>
+                                                <th>Fecha Final</th>
+                                                <th>Lider</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Obtener todos los proyecto
+                                            $resultado_proyectos = mysqli_query($conexion, "SELECT p.*, u.n_usuario, u.a_p, u.a_m 
+                                            FROM proyectos p JOIN usuarios u ON p.id_usuario = u.id_usuario WHERE u.id_rol LIMIT 2");
+                                            while ($proyecto = mysqli_fetch_assoc($resultado_proyectos)) : ?>
+                                                <tr>
+                                                    <td><?php echo $proyecto['id_proyecto']; ?></td>
+                                                    <td><?php echo $proyecto['n_proyecto']; ?></td>
+                                                    <td><?php echo $proyecto['descripcion']; ?></td>
+                                                    <td><?php echo $proyecto['fecha_inicio']; ?></td>
+                                                    <td><?php echo $proyecto['fecha_fin']; ?></td>
+                                                    <td><?php echo $proyecto['n_usuario'] . ' ' . $proyecto['a_p'] . ' ' . $proyecto['a_m']; ?></td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <a href="Dashboard_proyecto.php" class="btn btn-success btn-sm">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Cuadro de Equipos -->
+                    <div class="col-md-6">
+                        <div class="card text-dark bg-light mb-3 shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Equipos Creados por Líderes (Total: <?php echo $total_equipos; ?>)</h5>
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hover">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Proyecto</th>
+                                                <th>Lider</th>
+                                                <th>Descripción</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $resultado_equipos = mysqli_query($conexion, "SELECT e.*, p.n_proyecto, u.n_usuario
+                                            FROM equipos e JOIN usuarios u ON e.id_usuario = u.id_usuario
+                                            JOIN proyectos p ON e.id_proyecto = p.id_proyecto WHERE u.id_rol = 2 LIMIT 2");
+                                            while ($equipo = mysqli_fetch_assoc($resultado_equipos)) : ?>
+                                                <tr>
+                                                    <td><?php echo $equipo['id_equipo']; ?></td>
+                                                    <td><?php echo $equipo['n_proyecto']; ?></td>
+                                                    <td><?php echo $equipo['n_usuario']; ?></td>
+                                                    <td><?php echo $equipo['descripcion']; ?></td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <a href="Dashboard_equipo.php" class="btn btn-warning btn-sm">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 
     <!-- Bootstrap y JS -->
--- Lider/Dashboard.php
+++ Lider/Dashboard.php
@@ -34,6 +34,24 @@ if (!$Lider) {
 mysqli_close($conexion);
 ?>
 
+<?php
+// Conexión a la BD (reabrimos para las consultas)
+include '../connection/connection.php';
+
+// Contar proyectos
+$consulta_peroyectos = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM proyectos WHERE id_usuario = '$id'");
+$total_proyectos = mysqli_fetch_assoc($consulta_peroyectos)['total'];
+
+// Contar equipos
+$consulta_equipos = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM equipos WHERE id_usuario = '$id'");
+$total_equipos = mysqli_fetch_assoc($consulta_equipos)['total'];
+
+// Contar actividades
+$consulta_actividades = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM actividades_proyecto ap
+    JOIN proyectos p ON ap.id_proyecto = p.id_proyecto WHERE p.id_usuario = '$id'");
+$total_actividades = mysqli_fetch_assoc($consulta_actividades)['total'];
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -48,8 +66,13 @@ mysqli_close($conexion);
     <link rel="stylesheet" href="../Styles/style_dashboard.css">
 </head>
 
-<body>
+<style>
+    .card {
+        border-radius: 1rem;
+    }
+</style>
 
+<body>
     <!-- Sidebar -->
     <div class="sidebar">
         <h2>Lider Panel</h2>
@@ -75,8 +98,130 @@ mysqli_close($conexion);
                 </div>
             </div>
         </nav>
-    </div>
 
+        <!-- Proyectos Asignados -->
+        <div class="container mt-4">
+            <div class="row g-3">
+                <div class="row g-3">
+                    <div class="col-md-12">
+                        <div class="card text-dark bg-light mb-3 shadow">
+                            <div class="card-body">
+                                <h5 class="card-title"><b>Proyectos Asignados (Total: </b><?php echo $total_proyectos; ?>)</h5>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-hover">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th scope="col">ID Proyecto</th>
+                                                <th scope="col">Nombre Proyecto</th>
+                                                <th scope="col">Fecha de Inicio</th>
+                                                <th scope="col">Fecha de Fin</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Consulta para obtener los proyectos designado al líder
+                                            $consulta_proyectos = "SELECT * FROM proyectos WHERE id_usuario = '$id' LIMIT 3";
+                                            $resultado_proyectos = mysqli_query($conexion, $consulta_proyectos);
+                                            while ($proyecto = mysqli_fetch_assoc($resultado_proyectos)) : ?>
+                                                <tr>
+                                                    <td><?php echo $proyecto['id_proyecto']; ?></td>
+                                                    <td><?php echo $proyecto['n_proyecto']; ?></td>
+                                                    <td><?php echo $proyecto['fecha_inicio']; ?></td>
+                                                    <td><?php echo $proyecto['fecha_fin']; ?></td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <a href="Dashboard_proyecto.php" class="btn btn-primary btn-sm">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Equipos Creados por el Líder -->
+                    <div class="col-md-6">
+                        <div class="card text-dark bg-light mb-3 shadow">
+                            <div class="card-body">
+                                <h5><b>Equipos Creados (Total: </b><?php echo $total_equipos; ?>)</h5>
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hover">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th scope="col">ID Equipo</th>
+                                                <th scope="col">Proyecto</th>
+                                                <th scope="col">Miembros</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Consulta para obtener los equipos creados por el líder
+                                            $resultado_equipos = mysqli_query($conexion, "SELECT e.id_equipo, p.n_proyecto, GROUP_CONCAT(u.n_usuario SEPARATOR ', ') AS miembros
+                                            FROM equipos e JOIN proyectos p ON e.id_proyecto = p.id_proyecto JOIN detalle_equipos de ON e.id_equipo = de.id_equipo
+                                            JOIN usuarios u ON de.id_usuario = u.id_usuario WHERE e.id_usuario = '$id'
+                                            GROUP BY e.id_equipo, p.n_proyecto LIMIT 3");
+                                            // Verifica si la consulta tiene resultados
+                                            if ($resultado_equipos) {
+                                                while ($equipo = mysqli_fetch_assoc($resultado_equipos)) : ?>
+                                                    <tr>
+                                                        <td><?php echo $equipo['id_equipo']; ?></td>
+                                                        <td><?php echo $equipo['n_proyecto']; ?></td>
+                                                        <td><?php echo $equipo['miembros']; ?></td>
+                                                    </tr>
+                                            <?php endwhile;
+                                            } else {
+                                                echo "No se encontraron equipos.";
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <a href="Dashboard_equipo.php" class="btn btn-success btn-sm">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Actividades Creadas por el Líder -->
+                    <div class="col-md-6">
+                        <div class="card text-dark bg-light mb-3 shadow">
+                            <div class="card-body">
+                                <h5><b>Actividades Creadas (Total: </b><?php echo $total_actividades; ?>)</b></h5>
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hover">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th scope="col">ID Actividad</th>
+                                                <th scope="col">Actividad</th>
+                                                <th scope="col">Fecha Inical</th>
+                                                <th scope="col">Fecha Final</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Consulta para obtener las actividades asignadas al líder
+                                            $consulta_actividades = "SELECT ap.id_actividad_proyecto, ap.n_actividad, ap.descripcion, ap.fecha_inicio, ap.fecha_fin, ap.horas_estimadas, 
+                                            ap.horas_utilizadas, p.id_proyecto, p.n_proyecto AS nombre_proyecto, u.id_usuario, u.n_usuario AS nombre_responsable
+                                            FROM actividades_proyecto ap JOIN proyectos p ON ap.id_proyecto = p.id_proyecto JOIN usuarios u ON ap.id_usuario = u.id_usuario
+                                            WHERE p.id_usuario = '$id' LIMIT 3";
+                                            $resultado_actividades = mysqli_query($conexion, $consulta_actividades);
+                                            while ($actividad = mysqli_fetch_assoc($resultado_actividades)) : ?>
+                                                <tr>
+                                                    <td><?php echo $actividad['id_actividad_proyecto']; ?></td>
+                                                    <td><?php echo $actividad['n_actividad']; ?></td>
+                                                    <td><?php echo $actividad['fecha_inicio']; ?></td>
+                                                    <td><?php echo $actividad['fecha_fin']; ?></td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <a href="Dashboard_actividades.php" class="btn btn-warning btn-sm">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Bootstrap y JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
